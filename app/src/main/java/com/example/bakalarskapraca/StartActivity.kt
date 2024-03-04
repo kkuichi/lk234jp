f2526f4ca5f90aa334559261237419fe0752c56d
mac-pro <kuzykluka04@gmail.com>
2024-03-04 09:03:22 +0100

    start activity added TUKE commit

--- app/src/main/java/com/example/bakalarskapraca/StartActivity.kt
+++ app/src/main/java/com/example/bakalarskapraca/StartActivity.kt
@@ -1,5 +1,6 @@
 package com.example.bakalarskapraca
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,23 @@ class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+
+        val mSplashThread: Thread = Thread(){
+            run {
+                try {
+                    synchronized(this){
+                        Thread.sleep(1500)
+                    }
+                }catch (_:InterruptedException){
+                }finally {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+                    finish()
+                }
+            }
+        }
+
+        mSplashThread.start()
     }
 }
