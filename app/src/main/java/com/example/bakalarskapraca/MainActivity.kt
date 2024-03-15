407bc82ad1a2b23525c1083d507e60cba2796a9d
mac-pro <kuzykluka04@gmail.com>
2024-03-15 18:57:15 +0100

    progressBar for main commit

--- app/src/main/java/com/example/bakalarskapraca/MainActivity.kt
+++ app/src/main/java/com/example/bakalarskapraca/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ProgressBar
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var progressBar :ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +19,9 @@ class MainActivity : AppCompatActivity() {
         val teoriaButton: Button = findViewById(R.id.teoria_btn)
         val testyButton: Button = findViewById(R.id.testy_btn)
         val ulohyButton: Button = findViewById(R.id.ulohy_btn)
+        progressBar = findViewById(R.id.progressBar)
+
+        progressBar.progress = 75
 
 
         settingsButton.setOnClickListener {
