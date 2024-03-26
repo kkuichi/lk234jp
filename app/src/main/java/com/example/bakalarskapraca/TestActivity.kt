bbbbddb93cc1efa975b50ebc24bd3eb092c9ffae
mac-pro <kuzykluka04@gmail.com>
2024-03-26 16:59:04 +0100

    test style commit

--- app/src/main/java/com/example/bakalarskapraca/TestActivity.kt
+++ app/src/main/java/com/example/bakalarskapraca/TestActivity.kt
@@ -54,7 +54,15 @@ class QuizAdapter(private val quizList: List<QuizItem>, private val context: Con
 
     override fun onBindViewHolder(holder: QuizViewHolder, position: Int) {
         val quizItem = quizList[position]
-        holder.textView.text = "Test ${quizItem.quizNumber}"
+//        holder.textView.text = "Test ${quizItem.quizNumber}"
+        val resName = "Teoria_${position + 1}"
+        val resId = holder.itemView.context.resources.getIdentifier(resName, "string", holder.itemView.context.packageName)
+
+        if (resId != 0) { // Перевіряємо, що ресурс існує
+            holder.textView.text = holder.itemView.context.getString(resId)
+        } else {
+            holder.textView.text = "Test"
+        }
         holder.itemView.setOnClickListener {
             val intent = Intent(context, QuizActivity::class.java).apply {
                 putExtra("test_file", "test${quizItem.quizNumber}.json")
