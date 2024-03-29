3fe5602d8616a3e814d9073e81cf84727c9452df
mac-pro <kuzykluka04@gmail.com>
2024-03-29 09:41:45 +0100

    remove progress from priklady commit

--- app/src/main/java/com/example/bakalarskapraca/PdfViewerActivity.kt
+++ app/src/main/java/com/example/bakalarskapraca/PdfViewerActivity.kt
@@ -5,9 +5,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.RecyclerView
 import com.github.barteksc.pdfviewer.PDFView
 import java.io.IOException
 import kotlin.properties.Delegates
@@ -96,3 +102,44 @@ class PdfViewerActivity : AppCompatActivity() {
 
 
 }
+
+data class PdfItem(val id: Int, val name: String, val fileName: String, var progress: Int = 0, var lastPage: Int = 0)
+
+class PdfAdapter(
+    private val items: List<PdfItem>,
+    private val onItemClick: (PdfItem) -> Unit
+) : RecyclerView.Adapter<PdfAdapter.PdfViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PdfViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.teoria_item_layout, parent, false)
+        return PdfViewHolder(view, onItemClick)
+    }
+
+    override fun onBindViewHolder(holder: PdfViewHolder, position: Int) {
+        val item = items[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class PdfViewHolder(
+        itemView: View,
+        private val onItemClick: (PdfItem) -> Unit
+    ) : RecyclerView.ViewHolder(itemView) {
+        private val textView: TextView = itemView.findViewById(R.id.item_text)
+        private val progressNum: TextView = itemView.findViewById(R.id.teoria_progress_num)
+
+        fun bind(pdfItem: PdfItem) {
+            textView.text = pdfItem.name
+            if(pdfItem.progress == -2) { // Exception for Priklady activity
+                progressNum.visibility = View.GONE
+            }else {
+                progressNum.text = "${pdfItem.progress}%"
+            }
+            itemView.setOnClickListener {
+                onItemClick(pdfItem)
+            }
+        }
+    }
+}
+
--- app/src/main/java/com/example/bakalarskapraca/PrikladyActivity.kt
+++ app/src/main/java/com/example/bakalarskapraca/PrikladyActivity.kt
@@ -26,30 +26,27 @@ class PrikladyActivity : AppCompatActivity() {
         val pdfListView: RecyclerView = findViewById(R.id.pdfListView)
 
         val items = listOf(
-            PdfItem(1,getString(R.string.Priklady_1), "Priklady_1.pdf", 0, User.priklady_lastPage[0]),
-            PdfItem(2,getString(R.string.Priklady_2), "Priklady_2.pdf", 0, User.priklady_lastPage[1]),
-            PdfItem(3,getString(R.string.Priklady_3), "Priklady_3.pdf", 0, User.priklady_lastPage[2]),
-            PdfItem(4,getString(R.string.Priklady_4), "Priklady_4.pdf", 0, User.priklady_lastPage[3]),
-            PdfItem(5,getString(R.string.Priklady_5), "Priklady_5.pdf", 0, User.priklady_lastPage[4]),
-            PdfItem(6,getString(R.string.Priklady_6), "Priklady_6.pdf", 0, User.priklady_lastPage[5]),
-            PdfItem(7,getString(R.string.Priklady_7), "Priklady_6.pdf", 0, User.priklady_lastPage[6]),
+            PdfItem(1,getString(R.string.Priklady_1), "Priklady_1.pdf", -2, User.priklady_lastPage[0]),
+            PdfItem(2,getString(R.string.Priklady_2), "Priklady_2.pdf", -2, User.priklady_lastPage[1]),
+            PdfItem(3,getString(R.string.Priklady_3), "Priklady_3.pdf", -2, User.priklady_lastPage[2]),
+            PdfItem(4,getString(R.string.Priklady_4), "Priklady_4.pdf", -2, User.priklady_lastPage[3]),
+            PdfItem(5,getString(R.string.Priklady_5), "Priklady_5.pdf", -2, User.priklady_lastPage[4]),
+            PdfItem(6,getString(R.string.Priklady_6), "Priklady_6.pdf", -2, User.priklady_lastPage[5]),
+            PdfItem(7,getString(R.string.Priklady_7), "Priklady_6.pdf", -2, User.priklady_lastPage[6]),
 
         )
 
         startPdfViewerResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val data: Intent? = result.data
-                val progress = data?.getIntExtra("progress", 0) ?: 0
                 val fileName = data?.getStringExtra("fileName") ?: ""
                 val lastPage = data?.getIntExtra("lastPage", 0) ?: 0
 
-                // Update the progress in your list and refresh the RecyclerView
+                // Update the progress to USER and Firebase and refresh the RecyclerView
                 items.find { it.name == fileName }?.let { item ->
                     val itemId = item.id-1
-                    User.teoria_progress[itemId] = progress
-                    User.teoria_lastPage[itemId] = lastPage
+                    User.priklady_lastPage[itemId] = lastPage
                     User.uploadUserProgressToFireStore()
-                    item.progress = progress
                     item.lastPage = lastPage
                     pdfListView.adapter?.notifyDataSetChanged()
                 }
